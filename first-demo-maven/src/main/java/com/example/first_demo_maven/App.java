7f80a56701098aeba79d089666279d9b5494595e
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-06 13:13:51 +0100

    Proyecto maven creado con sus dependencias

---
+++ first-demo-maven/src/main/java/com/example/first_demo_maven/App.java
@@ -0,0 +1,20 @@
+package com.example.first_demo_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int i=0;
+        String tree = "";
+        while(i<30) {
+        	tree+="*";
+        	System.out.println(tree);
+        	i++;
+        }
+    }
+}
