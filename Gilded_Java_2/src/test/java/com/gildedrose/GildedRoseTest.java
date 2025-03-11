ea6095166d2fd56c214bf03b7d653e8a6dbd57ff
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-11 09:46:33 +0100

    Pruebas refactorizadas en clases Calculadora y Persona, inicio de pruebas GildedRose

--- Gilded_Java_2/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Gilded_Java_2/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,9 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
 
 class GildedRoseTest {
 
@@ -13,11 +16,44 @@ class GildedRoseTest {
 //        app.updateQuality();
 //        assertEquals("fixme", app.items[0].name);
 //    }
-    
+	
+	//-----------------------------------------------------------------------------------------------------
+	@Test
+	@DisplayName("Test degradación Sulfuras")
+	void testDowngradeSulfuras() {
+		Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 80) };
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	assertEquals(80, app.items[0].quality);
+	}
+	
+	@Test
+	@DisplayName("Test degradación Sulfuras con SellIn negativa y Quality menor")
+	void testDowngradeSulfurasNegative() {
+		Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 66) };
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	assertEquals(80, app.items[0].quality);
+	}
+	
+	
+	//-----------------------------------------------------------------------------------------------------
+	
+	@Test
+    @DisplayName("Test calidad superior a 50")
+    void testOverQuality() {
+    	Item[] items = new Item[] { new Item("Axe", 2, 55) };
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	assertEquals(50, app.items[0].quality);
+    }
+	
+	
     //Prueba paso dias baja quality y baja de uno en uno ya que no expira el SellIn
     @Test
-    public void testDowngradeQuality() {
-    	Item[] items = new Item[] { new Item("Potion", 5, 2) };
+    @DisplayName("Test degradación calidad normal")
+    void testDowngradeQuality() {
+    	Item[] items = new Item[] { new Item("Potion", 2, 5) };
     	GildedRose app = new GildedRose(items);
     	app.updateQuality();
     	assertEquals(4, app.items[0].quality);
@@ -25,8 +61,9 @@ class GildedRoseTest {
     
     //Prueba fecha SellIn Expirado
     @Test
-    public void testExpiredSellIn() {
-    	Item[] items = new Item[] { new Item("Potion", 10, 2) };
+    @DisplayName("Test degradación calidad con SellIn expirado")
+    void testExpiredSellIn() {
+    	Item[] items = new Item[] { new Item("Map", 2, 10) };
     	GildedRose app = new GildedRose(items);
 		for (int i=0; i<3; i++) {
 			app.updateQuality();
@@ -36,8 +73,9 @@ class GildedRoseTest {
     
 	//Prueba Quality negativa
 	@Test
-	public void testNegativeQuality() {
-	    Item[] items = new Item[] { new Item("Potion", 0, 2) };
+	@DisplayName("Test degradación calidad negativa")
+	void testNegativeQuality() {
+	    Item[] items = new Item[] { new Item("Sword", 2, 0) };
 	    GildedRose app = new GildedRose(items);
 		for (int i=0; i<3; i++) {
 			app.updateQuality();
@@ -47,9 +85,10 @@ class GildedRoseTest {
     
     //Prueba paso dias sube quality Aged Brie
     @Test
-    public void testAgedBrieQuality() {
+    @DisplayName("Test degradación calidad Queso Brie")
+    void testAgedBrieQuality() {
     	//Creamos un Queso Brie con Calidad 5 y SellIn 2
-    	Item[] items = new Item[] { new Item("Aged Brie", 5, 2) };
+    	Item[] items = new Item[] { new Item("Aged Brie", 2, 5) };
     	GildedRose app = new GildedRose(items);
 
     	app.updateQuality();
