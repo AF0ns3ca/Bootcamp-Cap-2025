77c28c135fd11620f194e350f5d4c724327c1a38
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-11 16:16:30 +0100

    Nivel superior de parametrizacion añadido, archivos en .csv

---
+++ Gilded_Java_2/src/test/java/com/gildedrose/GildedRoseTestCsv.java
@@ -0,0 +1,26 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GildedRoseTestCsv {
+
+	@ParameterizedTest(name = "{0} => sellIn: {1} quality: {2} -> sellIn: {3} quality: {4}")
+	@CsvFileSource(resources = "/items.csv", numLinesToSkip = 1)
+	void dataSourceTest(String producto, int sellIn, int quality, int sellInResult, int qualityResult) {
+		String name = producto.replace("\'","");
+		Item product = new Item(name, sellIn, quality);
+		GildedRose app = new GildedRose(new Item[] {
+			product	
+		});
+		app.updateQuality();
+		assertAll(name,
+				() -> assertEquals(name, product.name, "name"),
+				() -> assertEquals(sellInResult, product.sellIn, "sellIn"),
+				() -> assertEquals(qualityResult, product.quality, "quality")
+				);
+	}
+}
