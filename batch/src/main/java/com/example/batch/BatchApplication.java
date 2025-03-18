5f7e01dfc6e7fa3415e89aa3da37350d0f5877fe
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-18 15:37:36 +0100

    Spring Batch Csv, Xml y DB completado

---
+++ batch/src/main/java/com/example/batch/BatchApplication.java
@@ -0,0 +1,28 @@
+package com.example.batch;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+
+import com.example.models.Persona;
+import com.example.models.PersonaDTO;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
