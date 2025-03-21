436ec8a158fd36bd1ba70af5aedce82c1b7f8b3d
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-21 12:15:01 +0100

    Impors inutiles quitados

--- catalogov3/src/main/java/com/example/application/controllers/CategoryController.java
+++ catalogov3/src/main/java/com/example/application/controllers/CategoryController.java
@@ -4,9 +4,6 @@ package com.example.application.controllers;
 import java.net.URI;
 import java.util.List;
 
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,25 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.example.domains.contracts.services.ActorService;
 import com.example.domains.contracts.services.CategoryService;
-import com.example.domains.contracts.services.FilmService;
 import com.example.domains.entities.Category;
-import com.example.domains.entities.Film;
-import com.example.domains.entities.models.ActorDTO;
-import com.example.domains.entities.models.FilmDetailsDTO;
-import com.example.domains.entities.models.FilmShortDTO;
 import com.example.exceptions.BadRequestException;
 import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
