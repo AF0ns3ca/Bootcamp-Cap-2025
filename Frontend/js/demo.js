1a57f08c0f50c7145dc857b2c9b3615a4a274d85
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-26 16:25:03 +0100

    Cambios en web principal, añadidos ejemplos y pagina demo con su js

---
+++ Frontend/js/demo.js
@@ -0,0 +1,14 @@
+let txt = document.querySelector('#txtNombre');
+        let btn = document.querySelector('input[type="button"]');
+        let saludar = function() {
+            document.getElementById('outSaludo').textContent = `${txt.dataset.tipo} ${txt.value}`;
+        }
+        let registro = function() {
+            console.log(`Hola ${txt.value}`);
+        }
+        // btn.onClick = saludar;
+        btn.addEventListener('click', saludar);
+        btn.addEventListener('click', registro);
+        document.querySelector('input[value="Quitar"]').addEventListener('click', function() {
+            btn.removeEventListener('click', registro);
+        });
