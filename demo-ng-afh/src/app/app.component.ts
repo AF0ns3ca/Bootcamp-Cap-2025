f297df928bce731131fa08c2465f99b32b9839f7
AF0ns3ca <afonsecahdez@gmail.com>
2025-03-28 14:20:42 +0100

    Laboratorio de notificaciones completo, funcional y estilos en tailwind

--- demo-ng-afh/src/app/app.component.ts
+++ demo-ng-afh/src/app/app.component.ts
@@ -1,25 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggerService } from '@my/core';
+// import { NotificationComponent } from './main/notification/notification.component';
+import { DemosComponent } from './demos/demos.component';
+import { NotificationModalComponent } from './main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NotificationModalComponent, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-ng-afh';
 
-  modalVisible = false;
-
-  // Método para abrir el modal
-  openModal() {
-    this.modalVisible = true;
+  constructor(out: LoggerService) {
+    out.error('Es un error')
+    out.warn('Es un warn')
+    out.info('Es un info')
+    out.log('Es un log')
   }
-
-  // Método para cerrar el modal
-  closeModal() {
-    this.modalVisible = false;
-  }
-  
 }
